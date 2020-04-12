d7f8ee715704087d7eb2d1e29fea04d3e6401837
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-04-12 10:52:52 -0700

    added components

---
+++ employee-directory/src/Components/header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./header.css";
+
+class Header extends React.Component {
+  render() {
+    return <h1 className="jumbotron">Employee Directory!</h1>;
+  }
+}
+export default Header;
