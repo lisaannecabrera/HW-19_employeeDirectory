25901d313bfe85816446857f998229c444ef69a9
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-04-12 19:46:55 -0700

    troubleshooting

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import SearchContainer from "./Components/Searchcontainer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SearchContainer />
+    </div>
+  );
+}
+
+export default App;
